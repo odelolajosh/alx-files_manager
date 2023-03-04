293d81045ae64e46c4c30de3586d9c685781f392
odelolajosh <oladapoodelola@gmail.com>
2023-03-04 12:18:43 +0100

    feat: redis utils

---
+++ utils/redis.js
@@ -0,0 +1,62 @@
+import * as redis from 'redis';
+import { promisify } from 'util';
+
+/**
+ * Redis client
+ * @class RedisClient
+ */
+class RedisClient {
+	/**
+	 * Create a redis client
+	 * @constructor
+	 */
+	constructor() {
+		this.client = redis.createClient();
+		this.client.on('error', (err) => {
+			console.log('Redis error: ', String(err));
+		});
+		this._set = promisify(this.client.set).bind(this.client);
+		this._get = promisify(this.client.get).bind(this.client);
+		this._del = promisify(this.client.del).bind(this.client);
+	}
+
+	/**
+	 * Check if the client is connected to the database
+	 * @returns {boolean}
+	 */
+	isAlive() {
+		return this.client.connected;
+	}
+
+	/**
+	 * Get a value from the database
+	 * @return {Promise<string>}
+	 * @async
+	 */
+	async get(key) {
+		return await this._get(key);
+	}
+
+	/**
+	 * Set a value in the database
+	 * @param {string} key
+	 * @param {string} value
+	 * @param {number} duration
+	 * @async
+	 */
+	async set(key, value, duration) {
+		await this._set(key, value, 'EX', duration);
+	}
+
+	/**
+	 * Delete a value from the database
+	 * @param {string} key
+	 * @async
+	 */
+	async del(key) {
+		await this._del(key);
+	}
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
